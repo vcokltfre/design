f0d6e6ca04c9c7353f93d70e52e9d5ddddb0866f
vcokltfre <vcokltfre@gmail.com>
2023-05-07 00:33:25 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Design
+
+Just some web design stuff for learning and prototyping.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](./LICENSE) file for details.
---
+++ scripts/generate.py
@@ -0,0 +1,35 @@
+from json import loads, dumps
+from pathlib import Path
+from typing import TypedDict
+
+
+class Design(TypedDict):
+    name: str
+    description: str
+    url: str
+
+
+def generate() -> None:
+    designs_dir = Path("./designs")
+
+    designs: list[Design] = []
+
+    for design in designs_dir.iterdir():
+        if not design.is_dir():
+            continue
+
+        manifest_file = design / "design.json"
+        if not manifest_file.exists():
+            continue
+
+        data = loads(manifest_file.read_text())
+
+        designs.append(Design(**data, url=f"/designs/{design.name}"))
+
+    designs.sort(key=lambda design: design["name"])
+
+    Path("./static/meta/design-manifest.json").write_text(dumps(designs))
+
+
+if __name__ == "__main__":
+    generate()
